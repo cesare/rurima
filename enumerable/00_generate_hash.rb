bf388e893707c51a067b79762c84f4839ab92165
SAWADA Tadashi <cesare@mayverse.jp>
2012-08-11 16:55:25 +0900

    Solve naively

--- enumerable/00_generate_hash.rb
+++ enumerable/00_generate_hash.rb
@@ -5,11 +5,14 @@ expected = { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
 
 # (1) keyとvalueが交互に並んだ配列から
 kv = ['key1', 'value1', 'key2', 'value2', 'key3', 'value3']
+Hash(*kv)
 
 # (2) keyとvalueがペアになって並んだ配列から
 kvs = [['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']]
+kvs.inject({}){|h, obj| h[obj.first] = obj.last; h  }
+Hash[*kvs.flatten]
 
 # (3) keyの配列とvalueの配列から
 keys = ['key1', 'key2', 'key3']
 values =  ['value1', 'value2', 'value3']
-
+keys.zip(values).inject({}) {|h,pair| h[pair.first] = pair.last; h }
